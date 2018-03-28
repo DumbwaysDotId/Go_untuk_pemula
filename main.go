df0d591fc4c32531bf7ba5ae6943ab65b7cd81c6
radiegtya <radiegtya@yahoo.co.id>
2018-03-28 13:16:01 +0700

    function

--- main.go
+++ main.go
@@ -4,14 +4,25 @@ import (
 	"fmt"
 )
 
-func main() {
+func printAllVariables() {
 	var helloWorld string = "Hello World Go"
 	var fruitCount int = 2
 	var fruitPrice float64 = 2500.20
 	var isOldFruit bool = false
 
-	fmt.Println(helloWorld)
-	fmt.Println(fruitCount)
-	fmt.Println(fruitPrice)
-	fmt.Println(isOldFruit)
+	fmt.Printf("%v\n%v\n%v\n%v\n", helloWorld, fruitCount, fruitPrice, isOldFruit)
+}
+
+func helloWorld() string {
+	return "Hello World Go"
+}
+
+func add(firstNumber int, secondNumber int) int {
+	return firstNumber + secondNumber
+}
+
+func main() {
+	// printAllVariables()
+	// fmt.Println(helloWorld())
+	fmt.Println(add(3, 4))
 }
