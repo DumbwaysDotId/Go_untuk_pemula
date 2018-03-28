9caba47822c70675e1e28d6a568fe2158b544ca3
radiegtya <radiegtya@yahoo.co.id>
2018-03-28 13:24:40 +0700

    short variable

--- main.go
+++ main.go
@@ -4,25 +4,11 @@ import (
 	"fmt"
 )
 
-func printAllVariables() {
-	var helloWorld string = "Hello World Go"
-	var fruitCount int = 2
-	var fruitPrice float64 = 2500.20
-	var isOldFruit bool = false
-
-	fmt.Printf("%v\n%v\n%v\n%v\n", helloWorld, fruitCount, fruitPrice, isOldFruit)
-}
-
-func helloWorld() string {
-	return "Hello World Go"
-}
-
-func add(firstNumber int, secondNumber int) int {
-	return firstNumber + secondNumber
-}
+// var helloWorld string = "Hello World Go outside scope"
+// var helloWorld = "Hello World Go outside scope"
 
 func main() {
-	// printAllVariables()
-	// fmt.Println(helloWorld())
-	fmt.Println(add(3, 4))
+	helloWorld := "Hello World Go"
+	fruitCount := 5
+	fmt.Println("%v %v", helloWorld, fruitCount)
 }
