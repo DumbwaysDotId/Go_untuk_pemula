e0d41eaf3172a2b266fe0888791e155068665ce8
radiegtya <radiegtya@yahoo.co.id>
2018-03-28 13:05:03 +0700

    variables

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var helloWorld string = "Hello World Go"
+	var fruitCount int = 2
+	var fruitPrice float64 = 2500.20
+	var isOldFruit bool = false
+
+	fmt.Println(helloWorld)
+	fmt.Println(fruitCount)
+	fmt.Println(fruitPrice)
+	fmt.Println(isOldFruit)
+}
